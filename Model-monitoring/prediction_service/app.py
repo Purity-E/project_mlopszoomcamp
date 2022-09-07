a9abb95bf76a8a266465729794f469f8854d5ae5
Purity-E <purityeverniter@gmail.com>
2022-09-07 12:17:42 +0000

    monitoring update

--- Model-monitoring/prediction_service/app.py
+++ Model-monitoring/prediction_service/app.py
@@ -20,19 +20,18 @@ MONGODB_ADDRESS = os.getenv('MONGODB_ADDRESS', 'mongodb://127.0.0.1:27017')
 # getting the evidently address
 EVIDENTLY_SERVICE_ADDRESS = os.getenv('EVIDENTLY_SERVICE', 'http://127.0.0.1:5000')
 
-# count vectorizer
+# getting  count vectorizer and model
 CV_FILE = os.getenv('CV_FILE', 'CV.pkl')
-# loading count vectorizer
+MODEL_FILE = os.getenv('MODEL_FILE', 'model.pkl')
+
+
+# loading count vectorizer and model
 with open('CV_FILE', "rb") as f_in:
         cv = pickle.load(f_in)
 
-# loading the model from mlflow
-mlflow.set_tracking_uri("sqlite:///mlflow.db")
-mlflow.set_experiment("course_project")
+with open('MODEL_FILE', "rb") as f_in:
+        model = pickle.load(f_in)
 
-model_name = "FakeNews_Classifier"
-stage = "Production"
-model = mlflow.pyfunc.load_model(f"models:/{model_name}/{stage}")
 
 # normalizing the documents
 def normalize_document(doc):
@@ -98,7 +97,7 @@ app = Flask('News Classification')
 
 # saving to mongodb
 mongo_client = MongoClient(MONGODB_ADDRESS)
-db = mongo_client.get_database("prediction_service")
+db = mongo_client.get_database("news_classification")
 collection = db.get_collection("data")
 
 
@@ -116,7 +115,7 @@ def send_to_evidently_service(record, prediction):
 # creating function for the web request
 @app.route('/predict', methods=['POST'])
 def predict_endpoint():
-
+    
     record = request.get_json()
 
     doc = prepare_feature(record['title'])
